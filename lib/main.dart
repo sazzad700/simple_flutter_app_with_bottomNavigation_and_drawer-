38e8e817cfbe7b10d7b459643019a78868014669
sazzad <kazisazzad805@gmail.com>
2023-08-17 22:10:45 +0600

    the last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 main(){
@@ -20,7 +21,7 @@ class HomeActivity extends StatelessWidget{
 
   Mysnackbar(message,context){
     return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
+      SnackBar(content: Text(message),backgroundColor: Colors.green,)
     );
   }
 
@@ -28,10 +29,15 @@ class HomeActivity extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(title: const Text("SAZZADUL KOBIR"),),
+      appBar: AppBar(
+        title: const Text("SAZZADUL KOBIR"),
+        backgroundColor:Colors.green,
+      ),
      bottomNavigationBar: BottomNavigationBar(
+       selectedItemColor: Colors.green,
        currentIndex: 1,
        items: const [
+
          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
          BottomNavigationBarItem(icon: Icon(Icons.message),label: "Contact"),
          BottomNavigationBarItem(icon: Icon(Icons.person),label: "profile"),
@@ -49,13 +55,24 @@ class HomeActivity extends StatelessWidget{
       drawer: Drawer(
         child:  ListView(
           children: [
-            DrawerHeader(child: Text("Drawer Header")),
+            DrawerHeader(
+              padding: EdgeInsets.all(0),
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.green),
+                  accountName: Text("sazzadul kobir",style: TextStyle(fontSize: 20),),
+                  accountEmail: Text("The Great kazi"),
+                  currentAccountPicture: Image.network("https://cdn-icons-png.flaticon.com/128/2202/2202112.png"),
+
+                )
+            ),
 
              ListTile(
                 leading: Icon(Icons.home),
                 title: Text("home"),
                 onTap: (){
                   Mysnackbar("Home",context );
+                  //if user tuch a any item drawer will close
+                  Navigator.pop(context);
                 },
               ),
             ListTile(
@@ -63,6 +80,7 @@ class HomeActivity extends StatelessWidget{
               leading: Icon(Icons.phone),
               onTap: (){
                 Mysnackbar("Phone",context );
+                Navigator.pop(context);
               },
             ),
             ListTile(
@@ -70,6 +88,7 @@ class HomeActivity extends StatelessWidget{
               leading: Icon(Icons.contact_page),
               onTap: (){
                 Mysnackbar("contact",context );
+                Navigator.pop(context);
               },
             ),
             ListTile(
@@ -77,6 +96,7 @@ class HomeActivity extends StatelessWidget{
               leading: Icon(Icons.email),
               onTap: (){
                 Mysnackbar("Email",context );
+                Navigator.pop(context);
               },
             ),
           ],
